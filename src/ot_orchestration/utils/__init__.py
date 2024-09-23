9cf2d8da9591070f1c622f72008abd20f31ff90c
Szymon Szyszkowski <ss60@mib117351s.internal.sanger.ac.uk>
2024-09-23 06:29:31 +0100

    feat: functions to handle structured node configs

--- src/ot_orchestration/utils/__init__.py
+++ src/ot_orchestration/utils/__init__.py
@@ -9,8 +9,11 @@ from ot_orchestration.utils.path import (
     NativePath,
 )
 from ot_orchestration.utils.utils import (
+    chain_dependencies,
     check_gcp_folder_exists,
     clean_name,
+    convert_params_to_hydra_positional_arg,
+    find_node_in_config,
     random_id,
     read_hocon_config,
     read_yaml_config,
@@ -37,4 +40,7 @@ __all__ = [
     "create_batch_job",
     "extract_study_id_from_path",
     "URI_PATTERN",
+    "convert_params_to_hydra_positional_arg",
+    "find_node_in_config",
+    "chain_dependencies",
 ]
