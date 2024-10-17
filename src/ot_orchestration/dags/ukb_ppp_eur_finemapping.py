798bdc712dbf69d9bd674e8d736cd0ce049d62e0
Szymon Szyszkowski <ss60@mib117351s.internal.sanger.ac.uk>
2024-10-17 21:45:52 +0100

    feat(susie_finemapper): ensure proper output paths

--- src/ot_orchestration/dags/ukb_ppp_eur_finemapping.py
+++ src/ot_orchestration/dags/ukb_ppp_eur_finemapping.py
@@ -3,7 +3,6 @@
 from pathlib import Path
 
 from airflow.models.dag import DAG
-from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
 
 from ot_orchestration.operators.batch.finemapping import (
     FinemappingBatchJobManifestOperator,
@@ -47,18 +46,7 @@ with DAG(
     source_file_path = GCSPath(task_config["params"]["log_files_in"])
     destination_file_path = GCSPath(task_config["params"]["log_files_out"])
 
-    move_logs_job = GCSToGCSOperator(
-        task_id=task_config["id"],
-        source_bucket=source_file_path.bucket,
-        source_object=source_file_path.path,
-        match_glob=task_config["params"]["match_glob"],
-        destination_bucket=destination_file_path.bucket,
-        destination_object=destination_file_path.path,
-        move_object=True,
-    )
-
     tasks[generate_manifests.task_id] = generate_manifests
     tasks[finemapping_job.task_id] = finemapping_job
-    tasks[move_logs_job.task_id] = move_logs_job
 
     chain_dependencies(nodes=config["nodes"], tasks_or_task_groups=tasks)
