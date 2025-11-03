7d3e7df898c75f2cc5f3005abfb2aff2622fa2b1
project-defiant <szymonszyszkowski@gmail.com>
2025-11-03 11:10:05 +0000

    feat: update container

--- src/orchestration/operators/batch/manifest_generators/harmonisation.py
+++ src/orchestration/operators/batch/manifest_generators/harmonisation.py
@@ -200,7 +200,7 @@ class HarmonisationManifestGenerator(ProtoManifestGenerator):
         else:
             AirflowSkipException("No environments to create")
         # NOTE: Ensure the types are correct, as Environment requires dict[str,str] types.
-        return [{str(k): str(v) for k, v in row.items()} for row in var_list][0:10]
+        return [{str(k): str(v) for k, v in row.items()} for row in var_list][0:2000]
 
     @staticmethod
     def output_path(study: str, path_pattern: GCSPath) -> str:
