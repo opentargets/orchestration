b058e3a989a67a3890611da5fee0266648e9d506
Szymon Szyszkowski <69353402+project-defiant@users.noreply.github.com>
2026-03-27 11:15:21 +0000

    chore: update

---
+++ src/orchestration/models/environment/__init__.py
@@ -0,0 +1,42 @@
+"""Staging pipeline step class definition."""
+
+from __future__ import annotations
+
+from typing import Literal
+
+from pydantic import BaseModel
+
+type Env = Literal["Prod", "Test", "Dev"]
+
+
+class EnvSpec(BaseModel):
+    """Model representing the single environment specification ."""
+
+    name: Env
+    """Name of the environment, e.g. "Prod", "Test", etc."""
+    vars: dict[str, str]
+    """Dictionary of environment variable names and their corresponding values."""
+
+
+class Environments(BaseModel):
+    """Model representing all environments.
+
+    This class implements the registry of environment specs with `get_vars` method to retrieve the templating context for a specific environment.
+    """
+
+    environments: list[EnvSpec]
+    """List of environment specifications."""
+
+    def get_vars(self, env_name: Env) -> dict[str, str]:
+        """Get the environment variable values for the specified environment.
+
+        Args:
+            env_name: The environment to get the variable values for.
+
+        Returns:
+            A dictionary of environment variable names and their corresponding values for the specified environment.
+        """
+        for env_spec in self.environments:
+            if env_spec.name == env_name:
+                return env_spec.vars
+        raise ValueError(f"Environment '{env_name}' not found in configuration.")
---
+++ src/orchestration/models/pipeline/unified_pipeline_step.py
@@ -0,0 +1,81 @@
+"""Unified Pipeline step class definition."""
+
+from __future__ import annotations
+
+import logging
+from abc import abstractmethod
+
+from orchestration.dags.config.unified_pipeline import UnifiedPipelineConfig
+from orchestration.models.infrastructure.dataproc import ClusterDefinition
+from orchestration.models.pipeline.stage import PipelineStage
+
+
+class UnifiedPipelineStep:
+    """Unified Pipeline step class.
+
+    The Step class represents a step in an orchestration workflow.
+
+    This class is a stub for now, meant to be expanded with additional functionality
+    ported from other places in the orchestration codebase.
+
+    It is meant to be subclassed by more specific step implementations.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        config: UnifiedPipelineConfig,
+    ):
+        """Initialize a Step instance.
+
+        Args:
+            name (str): The name of the step.
+            config (AppConfig): The config object for the application the step uses.
+            logger (logging.Logger, optional): Logger instance for logging. Defaults to None.
+        """
+        self.name = name
+        """The full name of the step, including stage prefix."""
+        self.short_name = name.split("_", 1)[1]
+        """The short name of the step, without stage prefix."""
+        self.config = config
+        """The UnifiedPipelineConfig."""
+        self.stage = PipelineStage.from_step_name(name)
+        """The stage of the unified pipeline this step belongs to."""
+        self.runs_on_cluster = False
+        """Whether the step runs on a cluster."""
+        self.cluster_definition: ClusterDefinition
+        """The cluster definition for the step."""
+        self.logger = logging.getLogger(__name__)
+
+        self.logger.info(f"initializing {self.stage} step: {self.name}")
+
+    @property
+    def config_destination_prefix(self) -> str:
+        """Returns the URI prefix where the step config should be uploaded."""
+        return f"{self.config.dev_uri or self.config.release_uri}/etc/config"
+
+    @property
+    def bin_destination_prefix(self) -> str:
+        """Returns the URI prefix where the step binaries should be uploaded."""
+        return f"{self.config.dev_uri or self.config.release_uri}/etc/bin"
+
+    @property
+    @abstractmethod
+    def config_uri(self) -> str:
+        """Returns the URI for the step config."""
+
+    def get_cluster_definition(self, step_name) -> ClusterDefinition:
+        """Returns the cluster definition for the step, if applicable.
+
+        Raises:
+            ValueError: If the step does not run on a cluster.
+        """
+        if self.runs_on_cluster is False:
+            raise ValueError(f"step {step_name} does not run on a cluster.")
+
+        clusters = self.config.clusters.config.get("clusters", {})
+        sorted_cluster_names = sorted(clusters.keys(), key=len, reverse=True)
+        for cluster_name in sorted_cluster_names:
+            if step_name.startswith(cluster_name):
+                return ClusterDefinition(name=cluster_name, config=clusters[cluster_name])
+        raise ValueError(f"no cluster definition found for step {step_name}.")
