2959e798e5af915930acf0f616e6f07e7de0b9f8
Szymon Szyszkowski <69353402+project-defiant@users.noreply.github.com>
2026-06-01 15:48:57 +0100

    fix(dag_bag): reflect dag file location in dag_bag tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 from airflow.models import DagBag
 
 
-@pytest.fixture(params=["orchestration/dags"])
+@pytest.fixture(params=["src/orchestration/dags"])
 def dag_bag(request: pytest.FixtureRequest) -> DagBag:
     """Return a DAG bag for testing."""
     return DagBag(dag_folder=request.param, include_examples=False)
