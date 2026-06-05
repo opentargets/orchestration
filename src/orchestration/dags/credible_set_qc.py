21d7a96468d6f537e6ea63ef20a69a63a0caa28b
Szymon Szyszkowski <69353402+project-defiant@users.noreply.github.com>
2026-06-05 12:11:46 +0100

    refactor(staging): align usage of dataproc operators (#205)
    * refactor(staging): align usage of dataproc operators

--- src/orchestration/dags/credible_set_qc.py
+++ src/orchestration/dags/credible_set_qc.py
@@ -18,21 +18,18 @@ with DAG(
     description="Open Targets Genetics — CredibleSet QC ",
     default_args=shared_dag_args,
     **shared_dag_kwargs,
-):
+) as dag:
     tasks = {}
     for step in config["nodes"]:
         task = submit_gentropy_step(
             cluster_name=config["dataproc"]["cluster_name"],
             step_name=step["id"],
-            python_main_module=config["dataproc"]["python_main_module"],
             params=step["params"],
         )
         tasks[step["id"]] = task
 
     chain_dependencies(nodes=config["nodes"], tasks_or_task_groups=tasks)
-    dag = generate_dataproc_task_chain(
-        cluster_name=config["dataproc"]["cluster_name"],
-        cluster_init_script=config["dataproc"]["cluster_init_script"],
-        cluster_metadata=config["dataproc"]["cluster_metadata"],
-        tasks=list(tasks.values()),
-    )
+    generate_dataproc_task_chain(tasks=list(tasks.values()), **config["dataproc"])
+
+if __name__ == "__main__":
+    dag.test()
