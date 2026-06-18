dbd778d46a95eb59f1fa66716da75eed656631fd
Szymon Szyszkowski <69353402+project-defiant@users.noreply.github.com>
2026-06-18 10:42:42 +0100

    chore: update dataproc infastructure

--- src/orchestration/utils/dataproc.py
+++ src/orchestration/utils/dataproc.py
@@ -7,7 +7,6 @@ from typing import Any
 
 from airflow.models.baseoperator import BaseOperator
 from airflow.utils.trigger_rule import TriggerRule
-from google.cloud.dataproc_v1.types import DiskConfig
 
 from orchestration.operators.dataproc import (
     CreateClusterOperator,
